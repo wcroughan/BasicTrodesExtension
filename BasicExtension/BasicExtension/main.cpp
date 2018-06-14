54a530f8e91fff3d16dcb154e511066c2f1544c4
Bill Croughan <croughanb@gmail.com>
2018-06-14 16:15:27 -0700

    segfaulting in wsl on fopen

--- BasicExtension/BasicExtension/main.cpp
+++ BasicExtension/BasicExtension/main.cpp
@@ -1,6 +1,12 @@
 #include <stdarg.h>
 #include "TrodesNetwork/AbstractModuleClient.h"
 
+extern "C" {
+	__attribute__((visibility("default"))) void pyfun_end_first_run();
+	__attribute__((visibility("default"))) void pyfun_end_second_run();
+	__attribute__((visibility("default"))) int pyfun_start();
+}
+
 //Currently nothing special, but has potential!
 class TestClient : public AbstractModuleClient {
 public:
@@ -38,20 +44,25 @@ void log_msg(const char* msg, ...) {
 
 void log_msg(std::string msg) { log_msg(msg.c_str()); }
 
+bool IS_RUNNING = true;
+int pyfun_start() {
+	printf("hello, love!\n");
 
-int main() {
 	//create log file
-	std::string logfname = "test_extension_log.txt";
+	std::string logfname = "/home/wcroughan/test_extension_log.txt";
 	if ((logout = fopen(logfname.c_str(), "w")) == (FILE*)NULL) {
 		printf("Couldn't create log file\n");
 		return -1;
 	}
+	printf("Created log file\n");
+	log_msg("Hello, log!");
+
 	clock_gettime(CLOCK_REALTIME, &exec_start_time);
 
 	//connect
-	std::string CLIENT_ID = "";
-	std::string CLIENT_ADDR = "";
-	int CLIENT_PORT = 420;
+	const std::string CLIENT_ID = "CHOIR";
+	const std::string CLIENT_ADDR = "tcp://127.0.0.1";
+	const int CLIENT_PORT = 4220;
 	TestClient *client = new TestClient(CLIENT_ID.c_str(), CLIENT_ADDR.c_str(), CLIENT_PORT);
 	if (!client->initialize()) {
 		log_msg("Couldn't initialize network connection");
@@ -59,39 +70,205 @@ int main() {
 		return false;
 	}
 
-	client->sendTimeRateRequest();
 
+	std::string outfname = "neural_data_outfile";
+	FILE* nout;
+	if ((nout = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+	outfname = "neural_data_outfile_timestamps";
+	FILE* nout_ts;
+	if ((nout_ts = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+	outfname = "video_data_outfile_data";
+	FILE* vout_dat;
+	if ((vout_dat = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+	outfname = "neural_data_outfile_info";
+	FILE* vout_inf;
+	if ((vout_inf = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+	outfname = "neural_data_outfile2";
+	FILE* nout2;
+	if ((nout2 = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+	outfname = "neural_data_outfile_timestamps2";
+	FILE* nout_ts2;
+	if ((nout_ts2 = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+	outfname = "video_data_outfile_data2";
+	FILE* vout_dat2;
+	if ((vout_dat2 = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+	outfname = "neural_data_outfile_info2";
+	FILE* vout_inf2;
+	if ((vout_inf2 = fopen(outfname.c_str(), "wb")) == (FILE*)NULL) {
+		printf("couldn't open output file\n");
+		return -1;
+	}
+
+
+
+	client->sendTimeRateRequest();
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	const int TRODES_BUF_SIZE = 1024;
+	std::vector<std::string> chans;
+	const int num_tets = 6;
+	const int num_chans = num_tets * 4;
+	for (size_t i = 0; i < num_chans; i++)
+	{
+		chans.push_back(std::to_string(i));
+	}
+
+	NeuralConsumer *neucon = client->subscribeNeuralData(TRODES_BUF_SIZE, chans);
+	HFSubConsumer *vidcon = client->subscribeHighFreqData("PositionData", "CameraModule");
+
+	int16_t* neubuf = (int16_t*)malloc(sizeof(int16_t) * TRODES_BUF_SIZE * chans.size());
+	int vidsz = vidcon->getType().getByteSize();
+	char* vidbuf = (char*)malloc(vidsz);
+	log_msg("vidsz = %d", vidsz);
+
+	while (IS_RUNNING) {
+		size_t n = neucon->available(0);
+		for (int i = 0; i < n; i++)
+		{
+			timestamp_t ts = neucon->getData(neubuf);
+			fwrite(&ts, sizeof(timestamp_t), 1, nout_ts);
+			fwrite(neubuf, sizeof(int16_t), chans.size(), nout);
+		}
+		log_msg("neu: %d", n);
+
+		n = vidcon->available(0);
+		for (int i = 0; i < n; i++)
+		{
+			size_t retval = vidcon->readData(vidbuf, vidsz);
+			fwrite(vidbuf, sizeof(char), vidsz, vout_dat);
+			fwrite(&retval, sizeof(size_t), 1, vout_inf);
+		}
+		log_msg("vid: %d", n);
+
+		std::this_thread::yield();
+	}
+
+
+	client->unsubscribeHighFreqData("PositionData", "CameraModule");
+	client->unsubscribeHighFreqData(hfType_NEURO, TRODES_NETWORK_ID);
+	fclose(nout);
+	fclose(nout_ts);
+	fclose(vout_dat);
+	fclose(vout_inf);
+
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+
+
+	neucon = client->subscribeNeuralData(TRODES_BUF_SIZE, chans);
+	vidcon = client->subscribeHighFreqData("PositionData", "CameraModule");
+
+	IS_RUNNING = true;
+
+	while (IS_RUNNING) {
+		size_t n = neucon->available(0);
+		for (int i = 0; i < n; i++)
+		{
+			timestamp_t ts = neucon->getData(neubuf);
+			fwrite(&ts, sizeof(timestamp_t), 1, nout_ts2);
+			fwrite(neubuf, sizeof(int16_t), chans.size(), nout2);
+		}
+		log_msg("neu: %d", n);
+
+		n = vidcon->available(0);
+		for (int i = 0; i < n; i++)
+		{
+			size_t retval = vidcon->readData(vidbuf, vidsz);
+			fwrite(vidbuf, sizeof(char), vidsz, vout_dat2);
+			fwrite(&retval, sizeof(size_t), 1, vout_inf2);
+		}
+		log_msg("vid: %d", n);
+
+		std::this_thread::yield();
+	}
+
+	client->unsubscribeHighFreqData("PositionData", "CameraModule");
+	client->unsubscribeHighFreqData(hfType_NEURO, TRODES_NETWORK_ID);
+	fclose(nout2);
+	fclose(nout_ts2);
+	fclose(vout_dat2);
+	fclose(vout_inf2);
+
+
+	free(neubuf);
+	free(vidbuf);
+	delete client;
 	return 0;
 }
 
 void TestClient::recv_file_open(std::string filename)
 {
+	log_msg("recv_file_open: %s", filename.c_str());
 }
 
 void TestClient::recv_file_close()
 {
+	log_msg("recv_file_close");
 }
 
 void TestClient::recv_acquisition(std::string acquisition_type, uint32_t timestamp)
 {
+	log_msg("recv_acquisition: %s, %u", acquisition_type.c_str(), timestamp);
 }
 
 void TestClient::recv_source(std::string sourcename)
 {
+	log_msg("recv_source: %s", sourcename.c_str());
 }
 
 void TestClient::recv_quit()
 {
+	log_msg("recv_quit");
 }
 
 void TestClient::recv_time(uint32_t timestamp)
 {
+	log_msg("recv_time: %u", timestamp);
 }
 
 void TestClient::recv_timerate(int timerate)
 {
+	log_msg("recv_timerate: %d", timerate);
 }
 
 void TestClient::recv_event(std::string origin, std::string event, TrodesMsg & msg)
 {
+	log_msg("recv_event: %s, %s, %u", origin.c_str(), event.c_str(), msg.numContents());
+}
+
+void pyfun_end_first_run()
+{
+	IS_RUNNING = false;
+}
+
+void pyfun_end_second_run()
+{
+	IS_RUNNING = false;
 }
---
+++ BasicExtension/BasicGUI/BasicGUI.py
@@ -0,0 +1,98 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, QComboBox, QFileDialog, QInputDialog, QMessageBox, QLCDNumber
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QTimer, QTime
+import ctypes as cy
+
+CALLBACK = cy.CFUNCTYPE(None, cy.c_int)
+
+
+class App(QWidget):
+ 
+	def __init__(self):
+		super().__init__()
+		self.title = 'CHOIR: Computational Hippocampal Online Replay Detection'
+		self.initCConnection()
+		self.initUI()
+		
+	###
+	# UI Layout functions
+
+	def initUI(self):
+		self.left = 100
+		self.top = 100
+		self.width = 800
+		self.height = 400
+		self.setWindowTitle(self.title)
+		self.setGeometry(self.left, self.top, self.width, self.height)
+
+		self.state_var = 0
+
+		self.UIWelcome()
+		self.show()
+
+	def UIWelcome(self):
+		vbox = QVBoxLayout()
+		lbl_title = QLabel('Heres a simple lil program!')
+		vbox.addWidget(lbl_title)
+
+		hbox = QHBoxLayout()
+
+		self.loadbtn = QPushButton("Start first collection", self)
+		self.loadbtn.resize(self.loadbtn.sizeHint())
+		self.loadbtn.clicked.connect(self.welcomeloadbtn)
+		hbox.addStretch(1)
+		hbox.addWidget(self.loadbtn)
+		hbox.addStretch(1)
+
+		vbox.addLayout(hbox)
+		
+		if self.layout():
+			QWidget().setLayout(self.layout())
+		self.setLayout(vbox)
+
+	###
+	# Button functions
+
+	def welcomeloadbtn(self):
+		print("button pressed")
+		if self.state_var == 0:
+			print("tryina call cfunc")
+			self.pyfun_start()
+			print("called cfunc")
+			self.loadbtn.setTest('Stop first collection')
+			self.state_var = 1
+		elif self.state_var == 1:
+			self.pyfun_end_first_run()
+			self.loadbtn.setText('Stop second collection')
+			self.state_var = 2
+		else:
+			self.pyfun_end_second_run()
+
+	###
+	# C++ interaction functions
+	
+	def pyfun_start(self):
+		self.mydll.pyfun_start()
+
+	def pyfun_end_first_run(self):
+		self.mydll.pyfun_end_first_run()
+		
+	def pyfun_end_second_run(self):
+		self.mydll.pyfun_end_second_run()
+
+	def initCConnection(self):
+		self.mydll = cy.CDLL(r"libBasicExtension.so")
+		self.mydll.pyfun_start.argtypes = [];
+		self.mydll.pyfun_start.restype = cy.c_int
+		
+		self.mydll.pyfun_end_first_run.argtypes = [];
+		self.mydll.pyfun_end_first_run.restype = None
+		self.mydll.pyfun_end_second_run.argtypes = [];
+		self.mydll.pyfun_end_second_run.restype = None
+		
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = App()
+	sys.exit(app.exec_())
